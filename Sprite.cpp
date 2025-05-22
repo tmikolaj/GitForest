09bbe720db457e0e986e1ba44fb976b540159b2b
mikolaj <mikolajt55@gmail.com>
2025-05-22 19:15:14 +0200

    Added Sprite files (no methods implemented)

---
+++ Sprite.cpp
@@ -0,0 +1,8 @@
+#include "Sprite.h"
+
+Sprite::Sprite(std::string path) : m_path(path) {
+
+}
+void Sprite::instantiate(float x, float y, float w, float h) {
+    // Logic for instantiating a sprite
+}
---
+++ Sprite.h
@@ -0,0 +1,17 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <string>
+#include "pugixml.hpp"
+
+class Sprite {
+private:
+    std::string m_path;
+public:
+    Sprite(std::string path);
+    ~Sprite() = default;
+
+    void instantiate(float x, float y, float w, float h);
+};
+
+#endif //SPRITE_H
