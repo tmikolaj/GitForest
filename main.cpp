abee662fe0847c773651940ba533387ced1b75f7
mikolaj <mikolajt55@gmail.com>
2025-05-25 14:23:33 +0200

    Corrected path to sprites

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 int main() {
     // The current code is written only for testing purposes
     std::vector<float> positions = { 172, 258, 344, 433, 512, 602, 682 };
-    Sprite spruce("../spruce.svg");
-    Sprite cherryBlossomTree("../cherry-blossom-tree.svg");
+    Sprite spruce("../assets/spruce.svg");
+    Sprite cherryBlossomTree("../assets/cherry-blossom-tree.svg");
 
     Manager manager(spruce, cherryBlossomTree, positions);
     manager.init();
