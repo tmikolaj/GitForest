9b8e3ee4587385dc5fc25baca960cf64feb8db08
mikolaj <mikolajt55@gmail.com>
2025-05-28 10:38:18 +0200

    Improved instantiate method

--- src/Sprite.cpp
+++ src/Sprite.cpp
@@ -12,16 +12,28 @@ Sprite::Sprite(const Sprite &other) : Sprite(other.m_path) {
 }
 void Sprite::instantiate(float x, float y) {
     // Logic for instantiating a sprite
-    pugi::xml_node backgroundRoot = background.child("svg");
+    pugi::xml_node mainGroup = background.child("svg").child("g");
+
+    pugi::xml_node ref = mainGroup.first_child();
+    for (int i = 0; i < 9; i++) {
+        ref = ref.next_sibling();
+    }
+
+    pugi::xml_node spriteGroup;
+    if (ref) {
+        spriteGroup = mainGroup.insert_child_after("g", ref);
+    } else {
+        spriteGroup = mainGroup.append_child("g");
+    }
+
+    std::string transformStr = "translate(" + std::to_string(x) + ", " + std::to_string(y) + ")";
+    spriteGroup.append_attribute("transform") = transformStr.c_str();
+
     pugi::xml_node elementRoot = element.child("svg");
-
-    pugi::xml_node group = backgroundRoot.append_child("g");
-
-    std::string transformStr = "translate("+ std::to_string(x) + ", "+ std::to_string(y) + ")";
-    group.append_attribute("transform") = transformStr.c_str();
-
     for (pugi::xml_node child : elementRoot.children()) {
-        group.append_copy(child);
+        if (child.type() == pugi::node_element) {
+            spriteGroup.append_copy(child);
+        }
     }
     background.save_file("../assets/forest.svg");
 }
