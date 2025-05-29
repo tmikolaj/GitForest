ba7f416c036b481252cfb54fd0c81a0838811434
mikolaj <mikolajt55@gmail.com>
2025-05-29 19:22:52 +0200

    Deleted comment, vector is now initialized in JsonFileManager

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "src/Manager.h"
 
 int main() {
-    // The current code is written only for testing purposes
-    std::vector<float> positions = { 172, 258, 344, 433, 512, 602, 682 };
+    std::vector<float> positions;
     Sprite spruce("../assets/spruce.svg");
     Sprite cherryBlossomTree("../assets/cherry-blossom-tree.svg");
 
